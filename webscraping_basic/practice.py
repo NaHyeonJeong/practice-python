ec64e5654842cf1c814ee4fb576a9c3c9939de86
NaHyeonJeong <pigismile@gmail.com>
2021-01-31 22:20:59 +0900

    web scraping 2

--- webscraping_basic/practice.py
+++ webscraping_basic/practice.py
@@ -7,11 +7,22 @@ res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml") # lxml이라는 파서를 통해 BeautifulSoup 객체를 만듬, soup이 모든 정보를 가지고 있음
 
-imgs = soup.find_all("a", attrs={"class":"image"})
-for img in imgs:
-	print(img.img["src"])
+# imgs = soup.find_all("a", attrs={"class":"image"})
+# for img in imgs:
+# 	print(img.img["src"])
 
+# infos = soup.find_all("div", attrs={"class":"info"}) # find_all : list 형태로 반환
+# for info in infos:
+# 	txt = info.find("div").get_text()
+# 	print(txt)
 
-infos = soup.find_all("div", attrs={"class":"subject"}) # find_all : list 형태로 반환
-for info in infos:
-	print(info.get_text())
+print()
+print(">>> 150을 위한 옷 검색 결과 <<<")
+print()
+itemContents = soup.find_all("div", attrs={"class":"item-content"})
+for itemContent in itemContents:
+	pullLeft = itemContent.find("div", attrs={"class":"pull-left"}).get_text()
+	subject = itemContent.find("div", attrs={"class":"subject"}).get_text()
+	link = "https://sonyunara.com/" + itemContent.find("div", attrs={"class":"subject"}).a["href"]
+	print(pullLeft, end='')
+	print(subject, link)
